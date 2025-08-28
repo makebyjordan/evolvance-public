8036f21831aaa3c1eeb2a51325f439b1c70fb4c0
Julián Martínez Corral <julian@evol-vance.com>
2025-08-28 20:56:39 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8i-hGFLT_cRk_5F3KKL1d_l2_u3d1iWw",
-  authDomain: "evol-vance-landing2.firebaseapp.com",
-  projectId: "evol-vance-landing2",
-  storageBucket: "evol-vance-landing2.appspot.com",
-  messagingSenderId: "203459118563",
-  appId: "1:203459118563:web:7f4257c40d95738c3c4cd6",
+  "projectId": "evol-vance-landing2",
+  "appId": "1:203459118563:web:7f4257c40d95738c3c4cd6",
+  "storageBucket": "evol-vance-landing2.appspot.com",
+  "apiKey": "AIzaSyAE_3XYQsROWwLxmpzUYcjl96FB2JLVFcc",
+  "authDomain": "evol-vance-landing2.firebaseapp.com",
+  "messagingSenderId": "203459118563"
 };
 
 // Initialize Firebase
