5aae8d53cbd2488292e29983b3d1722495a87aa7
Evol-vance <admin@evol-vance.com>
2025-11-06 16:29:30 +0000

    quiero que aparezca asi en el mapa

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -69,7 +69,7 @@ export default function Footer() {
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                <div className="aspect-video relative rounded-lg overflow-hidden">
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.576832629765!2d-6.009366624141662!3d37.42236907206452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126b81867c2939%3A0x8e833446556e9960!2sTorre%202%2C%20C.%20Astronom%C3%ADa%2C%201%2C%2041015%20Sevilla!5e0!3m2!1ses!2ses!4v1700671373516!5m2!1ses!2ses"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.576832629765!2d-6.009366624141662!3d37.42236907206452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd126e1c27204f1f%3A0x3b64ea360ac9b33a!2sEvolvance!5e0!3m2!1ses!2ses!4v1700671373516!5m2!1ses!2ses"
                         width="100%"
                         height="100%"
                         style={{ border: 0 }}
